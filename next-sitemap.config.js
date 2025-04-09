e85a690753016bf6df751295f93852cf88196fd0
SerkanDiner <serkandiner1741@gmail.com>
2025-04-09 19:45:32 +0100

    fix the issue

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.experiencebook.com', // Replace with your real domain
+    siteUrl: 'https://www.thexperiencebook.com', // Replace with your real domain
     generateRobotsTxt: true,
     sitemapSize: 7000,
     changefreq: 'weekly',
