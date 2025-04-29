6be46924dc092deddac79109000beadd878f8217
SerkanDiner <serkandiner1741@gmail.com>
2025-04-29 17:59:43 +0100

    fixing some errros

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs";
+import { auth } from '@clerk/nextjs/server';
 import { connect } from "@/lib/mongodb/mongoose";
 import Post from "@/lib/models/post.model";
 import { handleNewPost } from "@/lib/actions/post";
