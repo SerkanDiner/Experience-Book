bca2ea962c423b3a412a36ea7447310e509c960b
SerkanDiner <serkandiner1741@gmail.com>
2025-04-29 01:35:54 +0100

    ....

---
+++ src/app/post/loading.jsx
@@ -0,0 +1,13 @@
+// src/app/loading.jsx
+import { FaSpinner } from 'react-icons/fa';
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center bg-white dark:bg-gray-900">
+      <FaSpinner className="animate-spin text-orange-500 text-5xl mb-4" />
+      <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+        Loading your experience...
+      </p>
+    </div>
+  );
+}
